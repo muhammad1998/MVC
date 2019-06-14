05697c8744391b8bd9ccdd24da015acea5f9d19c
Syed Ammar Ali <16b-048-bs@uit.edu>
2019-06-14 15:52:53 +0500

    first

---
+++ WebApplication3/WebApplication3/Controllers/CheckController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class CheckController : Controller
+    {
+        //
+        // GET: /Check/
+        public ActionResult Index()
+        {
+            MBH m = new MBH();
+            m.abc = 1234;
+
+            return View("Index",m);
+        }
+	}
+}
---
+++ WebApplication3/WebApplication3/Models/MBH.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class MBH
+    {
+        public int abc{ get; set; }
+
+
+    }
+}
